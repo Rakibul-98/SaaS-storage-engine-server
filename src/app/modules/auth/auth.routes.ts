e9d6e6c7cd7591379bb0623470efd2abdcf6fe0c
Rakibul-98 <rakibul.rupom2001@gmail.com>
2026-04-16 23:27:12 +0600

    added rate limiter to auth route and update get dashboard statistics service

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -5,24 +5,27 @@ import {
   loginValidationSchema,
   registerValidationSchema,
 } from "./auth.validation";
+import { authRateLimiter } from "../../middlewares/rateLimiter";
 
 const router = express.Router();
 
 router.post(
   "/register",
+  authRateLimiter,
   validateRequest(registerValidationSchema),
   AuthController.register,
 );
 
 router.post(
   "/login",
+  authRateLimiter,
   validateRequest(loginValidationSchema),
   AuthController.login,
 );
 
 router.get("/verify-email", AuthController.verifyEmail);
 
-router.post("/forgot-password", AuthController.forgotPassword);
+router.post("/forgot-password", authRateLimiter, AuthController.forgotPassword);
 
 router.post("/reset-password", AuthController.resetPassword);
 
